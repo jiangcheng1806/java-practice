12beae0a1b6b1b061b34f8b5f8e86607f3174f1e
jiangcheng <jiangcheng1806@163.com>
2023-03-28 14:43:26 +0800

    查看基本数据类型

---
+++ jdk-practices/src/main/java/com/jiangc/practice/jdk/annotation/exec/EnumTest.java
@@ -0,0 +1,10 @@
+package com.jiangc.practice.jdk.annotation.exec;
+
+public class EnumTest {
+
+    public enum TrafficLamp {
+        GREEN,
+        RED,
+        YELLOW
+    }
+}
---
+++ redis-practices/src/main/java/com/jiangc/practice/redis/utils/RedisLockCallback.java
@@ -0,0 +1,10 @@
+package com.jiangc.practice.redis.utils;
+
+/**
+ * RedisTemplate的回调方法
+ * @author 李宁
+ *
+ */
+public interface RedisLockCallback {
+	public Object doInRedisLock();
+}
